bb76fd3e046cada44e9f3bdef9b060f17cedbae3
frankaeguana <franka.eiffel@aeguana.com>
2022-07-18 19:01:50 -0300

    fix init.sh

--- install/kubeadm-init.sh
+++ install/kubeadm-init.sh
@@ -4,20 +4,21 @@ echo "Initializing kubeadm init"
 read -p "Enter the ip of controller node: " controller_ip
 
 # Create kubeadm-config.yaml
-cat << 'EOF' > /usr/local/kubeadm-config.yaml
+cat << EOF > ./kubeadm-config.yaml
 kind: ClusterConfiguration
 apiVersion: kubeadm.k8s.io/v1beta3
 kubernetesVersion: v1.24.0
 networking:
-  podSubnet: "$controller_ip" # --pod-network-cidr
+  podSubnet: "${controller_ip}" # --pod-network-cidr
 # controlPlaneEndpoint: <ip/dns> # --control-plane-endpoint (load balancer ip/dns)
 ---
 kind: KubeletConfiguration
 apiVersion: kubelet.config.k8s.io/v1beta1
 cgroupDriver: systemd
 EOF
+
 echo " "
-sudo kubeadm init --config=/usr/local/kubeadm-config.yaml
+sudo kubeadm init --config=./kubeadm-config.yaml
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config

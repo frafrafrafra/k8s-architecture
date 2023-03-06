d202dd8810e89b21e709a517dee98ed4f880ce9d
frankaeguana <franka.eiffel@aeguana.com>
2023-03-05 21:22:24 -0300

    fix

--- install/basic_node_config.sh
+++ install/basic_node_config.sh
@@ -1,4 +1,4 @@
 
 echo "Setup ssh configuration"
-sed -i 's/.*ClientAliveInterval.*/ClientAliveInterval 60/' /etc/ssh/sshd_config
-service ssh restart
+sed -i 's/.*ClientAliveInterval.*/ClientAliveInterval 1800/' /etc/ssh/sshd_config
+systemctl restart ssh
